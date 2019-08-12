5dc4609d7e203bf864601f644fe6fadbee8b3303
yukinaka <yuki.n.0918@gmail.com>
2019-08-12 21:50:43 +0900

    add morgan

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var http = require('http');
+var morgan = require('morgan');
 var express = require('express');
 var app = express();
 
+app.use(morgan('combined'));
 app.get('/', function(req, res) {
     return res.send(200)
 })
 
 var server = http.createServer(app);
 server.listen(3000)
+console.log('3000');
 
 // var server = http.createServer(function (req, res) {
 //     console.dir(req, res);
