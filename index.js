a97010705afe03953380367f23d439c58a36cc8c
yukinaka <yuki.n.0918@gmail.com>
2019-08-12 22:28:02 +0900

    create form

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var http = require('http');
+var bodyParser = require('body-parser')
 var morgan = require('morgan');
 var express = require('express');
 var app = express();
@@ -7,10 +8,25 @@ var app = express();
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(morgan('combined'));
+
 app.get('/', function(req, res) {
     return res.render('index', {title: 'express practice'})
 })
-app.use(morgan('combined'));
+
+app.get('/form', function (req, res) {
+    return res.render('form');
+})
+
+app.post('/form', function (req, res) {
+    return res.render('result',
+      {
+          username: req.body.username,
+          message: req.body.message,
+      }
+  )
+})
 
 var server = http.createServer(app);
 server.listen(3000)
