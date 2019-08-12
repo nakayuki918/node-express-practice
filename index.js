b9f483b3c2d3d70029acb8be0a15cbc8f1b5ea45
yukinaka <yuki.n.0918@gmail.com>
2019-08-12 22:08:56 +0900

    change template engine

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     return res.render('index', {title: 'express practice'})
