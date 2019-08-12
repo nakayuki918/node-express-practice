c8c8f35f80141a66ebf9be09842ce7336fc7327e
yukinaka <yuki.n.0918@gmail.com>
2019-08-12 22:05:05 +0900

    add template file

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+var path = require('path');
 var http = require('http');
 var morgan = require('morgan');
 var express = require('express');
 var app = express();
 
-app.use(morgan('combined'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-    return res.send(200)
+    return res.render('index', {title: 'express practice'})
 })
+app.use(morgan('combined'));
 
 var server = http.createServer(app);
 server.listen(3000)
 console.log('3000');
-
-// var server = http.createServer(function (req, res) {
-//     console.dir(req, res);
-//     res.writeHead(200, { 'ContentType': 'text/plain' });
-//     res.end('HelloWorld');
-// });
